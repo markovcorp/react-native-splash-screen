01b1154ca50c636cc55908c444f5d8faa6a1d280
Billy Pham <billypham08@gmail.com>
2018-12-12 10:03:37 -0800

    Add full screen flag

--- android/src/main/java/org/devio/rn/splashscreen/SplashScreen.java
+++ android/src/main/java/org/devio/rn/splashscreen/SplashScreen.java
@@ -31,9 +31,17 @@ public class SplashScreen {
                     mSplashDialog = new Dialog(activity, themeResId);
                     mSplashDialog.setContentView(R.layout.launch_screen);
                     mSplashDialog.setCancelable(false);
+                    mSplashDialog.getWindow().setFlags(WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE, WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE);
 
                     if (!mSplashDialog.isShowing()) {
                         mSplashDialog.show();
+                        mSplashDialog.getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
+                            || View.SYSTEM_UI_FLAG_FULLSCREEN
+                            || View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            || View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                            || View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            || View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+                        mSplashDialog.getWindow().clearFlags(WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE);
                     }
                 }
             }
